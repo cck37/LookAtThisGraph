c9fe41e499fdfe7946e51f2b2c12ca60f87ec59d
cck37 <ckennedy0323@gmail.com>
2022-12-29 17:25:36 -0500

    fix: added selection to circle packing

--- look-at-this-graph/src/components/charts/KHCirclePacking.vue
+++ look-at-this-graph/src/components/charts/KHCirclePacking.vue
@@ -1,92 +1,72 @@
 <template>
   <div>
-    <h1>Circle Pack in D3</h1>
+    <v-select
+      clearable
+      chips
+      hint="What recipes do you still need to finish"
+      :items="recipes"
+      label="Recipes"
+      multiple
+      variant="underlined"
+      v-model="selectedRecipes"
+    ></v-select>
     <svg width="960" height="960"></svg>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-import { mapToHierarchy, mapToFlat } from "./mapper";
+import { watchEffect, ref, onMounted } from "vue";
+import { mapToFlat } from "./mapper";
 import * as d3 from "d3";
 
 const props = defineProps(["khData"]);
-
-function chart() {
-  var height = 932;
-  var width = 932;
-
-  var pack = d3.pack().size([900, 900]);
-
-  function round(value, decimals) {
-    return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
-  }
-
-  const sumMaxDropRate = (group) => {
-    return group.reduce((x, y) => round(x * (1 - y.drop_rate_max), 2), 1);
-  };
-
-  let data = mapToFlat(props.khData).filter(
-    (d) => d.drop_rate_max !== undefined
+let data = mapToFlat(props.khData);
+const recipes = [...new Set(data.map((comp) => comp.recipeName))]; // arr->set->arr to remove dupes
+const selectedRecipes = ref([]);
+watchEffect(() => {
+  chart(
+    data.filter((comp) =>
+      selectedRecipes.value.some((r) => r === comp.recipeName)
+    )
   );
-  let groups = d3.rollup(
-    data,
-    sumMaxDropRate,
-    function (d) {
-      return d.locationName;
-    },
-    function (d) {
-      return d.componentName;
-    }
-  );
+});
 
-  let root_hierarchy = d3.hierarchy(groups);
-  root_hierarchy.sum((d) => 1 - d[1]);
+function color() {
+  d3.scaleLinear()
+    .domain([0, 5])
+    .range(["hsl(0, 0%, 10%)", "hsla(0, 0%, 80%)"])
+    .interpolate(d3.interpolateHcl);
+}
 
-  console.table(groups);
-  console.log(root_hierarchy);
-  console.log(this);
-  const root = pack(root_hierarchy);
-  console.log(root.descendants());
+function update(root) {
+  const height = 932;
+  const width = 932;
   let focus = root;
   let view;
 
-  const color = d3
-    .scaleLinear()
-    .domain([0, 5])
-    .range(["hsla(0, 0%, 10%, 1)", "hsla(0, 0%, 80%, 0.25)"])
-    .interpolate(d3.interpolateHcl);
-
   const svg = d3
     .select("svg")
-    .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+    .attr("viewBox", [-(width / 2), -(height / 2), width, height])
     .style("display", "block")
     .style("margin", "0 -14px")
     .style("background", color(0))
     .style("cursor", "pointer")
     .on("click", (event) => zoom(event, root));
 
-  const node = svg
-    .append("g")
+  const node = d3
+    .select("svg")
     .selectAll("circle")
     .data(root.descendants().slice(1))
     .join("circle")
     .attr("fill", (d) => (d.children ? color(d.depth) : "white"))
-    .attr("pointer-events", (d) => (!d.children ? "none" : null))
-    .on("mouseover", function () {
-      d3.select(this).attr("stroke", "#000");
-    })
-    .on("mouseout", function () {
-      d3.select(this).attr("stroke", null);
-    })
+    .attr("pointer-events", (d) => (!d.children ? "none" : "auto"))
     .on(
       "click",
       (event, d) => focus !== d && (zoom(event, d), event.stopPropagation())
     );
 
-  const label = svg
-    .append("g")
-    .style("font", "10px sans-serif")
+  const label = d3
+    .select("svg")
     .attr("pointer-events", "none")
     .attr("text-anchor", "middle")
     .selectAll("text")
@@ -94,16 +74,14 @@ function chart() {
     .join("text")
     .style("fill-opacity", (d) => (d.parent === root ? 1 : 0))
     .style("display", (d) => (d.parent === root ? "inline" : "none"))
+    .style("font-size", (d) => (d.parent === root ? `${d.r * 0.4}px` : "12px"))
     .text((d) =>
       d.parent === root
         ? d.data[0]
         : `${d.data[0]} - ${parseFloat(d.value).toFixed(3) * 100}%`
     );
 
-  console.log(label);
-  zoomTo([root.x, root.y, root.r * 2]);
-
-  function zoomTo(v) {
+  const zoomTo = (v) => {
     const k = width / v[2];
 
     view = v;
@@ -117,14 +95,15 @@ function chart() {
       (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
     );
     node.attr("r", (d) => d.r * k);
-  }
+  };
 
   function zoom(event, d) {
     const focus0 = focus;
 
     focus = d;
 
-    const transition = svg
+    const transition = d3
+      .select("svg")
       .transition()
       .duration(event.altKey ? 7500 : 750)
       .tween("zoom", (d) => {
@@ -146,90 +125,54 @@ function chart() {
       });
   }
 
-  return svg.node();
+  zoomTo([root.x, root.y, root.r * 2]);
+}
+
+// TODO: Mimic https://observablehq.com/@d3/pack (clip-path, text structuring, etc.)
+function chart(data) {
+  const height = 932;
+  const width = 932;
+  var pack = d3.pack().size([900, 900]);
+
+  const round = (value, decimals) => {
+    return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+  };
+
+  const sumMaxDropRate = (group) => {
+    return group.reduce((x, y) => round(x * (1 - y.drop_rate_max), 2), 1);
+  };
+
+  console.log(data);
+
+  const groups = d3.rollup(
+    data,
+    sumMaxDropRate,
+    function (d) {
+      return d.locationName;
+    },
+    function (d) {
+      return d.componentName;
+    }
+  );
+
+  const root_hierarchy = d3
+    .hierarchy(groups)
+    .sum((d) => 1 - d[1])
+    .sort((a, b) => d3.descending(a.value, b.value));
+
+  const root = pack(root_hierarchy);
+
+  update(root);
 }
 
 onMounted(() => {
-  chart();
-  //   var tooltip = d3
-  //     .select(".tooltip-area")
-  //     .style("opacity", 0)
-  //     .attr("class", "tooltip")
-  //     .style("border", "solid");
-
-  //   const mouseover = (_event, _d) => {
-  //     tooltip.style("opacity", 1);
-  //   };
-
-  //   const mouseleave = (_event, _d) => {
-  //     tooltip.style("opacity", 0);
-  //   };
-
-  //   const mousemove = (event, d) => {
-  //     const text = d3.select(".tooltip-area__text");
-  //     text.text(
-  //       d.children === undefined
-  //         ? `${d.data[0] ?? ""}: ${parseFloat(d.value).toFixed(3) * 100}%`
-  //         : ""
-  //     );
-
-  //     //const [x, y] = d3.pointer(event, d);
-
-  //     tooltip.attr("transform", `translate(${d.x}, ${d.y + 20})`);
-  //   };
-
-  //   function zoomed({ transform }) {
-  //     nodes.attr("transform", transform);
-  //   }
-
-  //   var nodes = d3
-  //     .select("svg")
-  //     .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-  //     .style("display", "block")
-  //     .selectAll(".plot_area")
-  //     .data(root.descendants())
-  //     .join("g")
-  //     .attr("transform", function (d) {
-  //       return "translate(" + [d.x, d.y] + ")";
-  //     });
-
-  //   nodes
-  //     .append("circle")
-  //     .attr("r", function (d) {
-  //       return d.r;
-  //     })
-  //     .attr("class", "node");
-
-  //   nodes
-  //     .append("text")
-  //     .attr("dy", 4)
-  //     .text(function (d) {
-  //       return d.children !== undefined && d.data[0]
-  //         ? `${d.data[0]}: ${parseFloat(d.value).toFixed(3)}`
-  //         : "";
-  //     });
-
-  //   nodes
-  //     .on("mouseover", mouseover)
-  //     .on("mousemove", mousemove)
-  //     .on("mouseleave", mouseleave)
-  //     .call(
-  //       d3
-  //         .zoom()
-  //         .extent([
-  //           [0, 0],
-  //           [width, height],
-  //         ])
-  //         .scaleExtent([1, 10])
-  //         .on("zoom", zoomed)
-  //     );
+  chart(data);
 });
 </script>
 
 <style>
 .chart > div {
   white-space: nowrap;
-  font: 10px sans-serif;
   text-align: right;
   padding: 3px;
   margin: 1px;
@@ -244,9 +187,6 @@ circle {
 text {
   fill: gold;
   text-anchor: middle;
-}
-.tooltip-area__text {
-  font-size: 12px;
 }
 
 .node:hover {
--- look-at-this-graph/src/views/kh/KH1.vue
+++ look-at-this-graph/src/views/kh/KH1.vue
@@ -1,5 +1,11 @@
 <template>
-  <KHCirclePacking v-if="khData" :khData="khData" />
+  <v-card class="pa-6 rounded-sm mx-12 my-12">Kingdom Hearts 1</v-card>
+  <v-card class="pa-6 rounded-sm mx-12 my-12">
+    <v-card-title>"Highest" spawn rates by location</v-card-title>
+    <v-card-text>
+      <KHCirclePacking v-if="khData" :khData="khData" />
+    </v-card-text>
+  </v-card>
 </template>
 
 <script async setup>
@@ -8,6 +14,7 @@ import { ref, onServerPrefetch, onMounted } from "vue";
 import KHCirclePacking from "../../components/charts/KHCirclePacking.vue";
 
 const khData = ref(null);
+const whatever = [];
 
 onServerPrefetch(async () => {
   // component is rendered as part of the initial request
